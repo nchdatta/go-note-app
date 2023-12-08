dcbb4f17277bd3129112efbb2a0de30b0d2941b2
Nayan Chandra Datta <nayanchdatta02@gmail.com>
2023-12-08 21:21:47 +0600

    initial commit

---
+++ config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func Config(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Default().Println("Error loading the .env file.")
+	}
+	return os.Getenv(key)
+}
